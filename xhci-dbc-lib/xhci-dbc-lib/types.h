bcaeb7aa8e68af77e7bb7b54a0c24f5a7fc5ab6b
LuminousTrout <199683198+LuminousTrout@users.noreply.github.com>
2025-02-17 18:27:21 -0700

    unzipgit

---
+++ xhci-dbc-lib/xhci-dbc-lib/types.h
@@ -0,0 +1,26 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include "pstdint.h"
+
+#ifndef _In_
+#define _In_
+#endif
+#ifndef _Out_
+#define _Out_
+#endif
+
+#ifndef TRUE
+#define TRUE    1
+#endif
+#ifndef FALSE
+#define FALSE   0
+#endif
+
+#ifndef NULL
+#define NULL ((VOID *) 0)
+#endif
+
+typedef uint8_t bool;
+
+#endif // __TYPES_H__
